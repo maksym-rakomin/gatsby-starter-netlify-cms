6f5943022725db8678dbc98d8e5960b48f2374f6
Maksym Rakomin <maksym.rakomin@gmail.com>
2022-11-25 17:45:22 +0200

    update page new

--- src/cms/preview-templates/NewPagePreview.js
+++ src/cms/preview-templates/NewPagePreview.js
@@ -2,31 +2,18 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { NewPageTemplate } from '../../templates/new-page'
 
-const NewPagePreview = ({ entry, getAsset }) => {
-  const data = entry.getIn(['data']).toJS()
-
-  if (data) {
-    return (
-      <NewPageTemplate
-        image={getAsset(data.image)}
-        title={data.title}
-        heading={data.heading}
-        subheading={data.subheading}
-        description={data.description}
-        intro={data.intro || { blurbs: [] }}
-        mainpitch={data.mainpitch || {}}
-      />
-    )
-  } else {
-    return <div>Loading...</div>
-  }
-}
+const NewPagePreview = ({ entry, widgetFor }) => (
+    <NewPageTemplate
+        title={entry.getIn(['data', 'title'])}
+        content={widgetFor('body')}
+    />
+)
 
 NewPagePreview.propTypes = {
   entry: PropTypes.shape({
     getIn: PropTypes.func,
   }),
-  getAsset: PropTypes.func,
+  widgetFor: PropTypes.func,
 }
 
 export default NewPagePreview
